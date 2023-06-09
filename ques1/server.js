6f85566b9f0fb223b1cb71807f6ebec6506b6313
Anshdeep singh <anshdeep0singh@egmail.com>
2023-06-09 22:23:58 +0530

    Screenshot

--- ques1/server.js
+++ ques1/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const axios = require('axios');
 const https = require('https');
+const cron = require('node-cron');
 require('dotenv').config();
 
 const app = express();
 const API_URL = 'http://104.211.219.98/train';
 const AUTH_API_URL = 'https://104.211.219.98/train/auth';
 
-// const authorizationToken = process.env.AUTH_TOKEN;
 let authorizationToken = '';
+let trainSchedule = [];
 
-const fetchAuthorizationToken = async function registerCompany() {
+// Fetch authorization token from the API
+const fetchAuthorizationToken = async () => {
   try {
     const requestData = {
       companyName: 'Travel Buddy',
@@ -23,84 +25,97 @@ const fetchAuthorizationToken = async function registerCompany() {
 
     const agent = new https.Agent({ rejectUnauthorized: false });
 
-    const response = await axios.post(AUTH_API_URL, requestData, { httpsAgent: agent });;
-    const { access_token } = response.data;
-    authorizationToken = `Bearer ${access_token}`;
-    // authorizationToken = response.data.access_token;
+    const response = await axios.post(AUTH_API_URL, requestData, { httpsAgent: agent });
+    authorizationToken = `Bearer ${response.data.access_token}`;
+
+    // Fetch train schedules after obtaining the authorization token
+    fetchTrainSchedules();
   } catch (error) {
     console.error(error);
   }
-}
+};
 
-// Fetch the authorization token on server startup
-fetchAuthorizationToken();
-
-app.get('/trains', async (req, res) => {
+// Fetch train schedules from the John Doe Railway Server
+const fetchTrainSchedules = async () => {
   try {
-    // Fetch authorization token if not available
+    // Make sure authorization token is available
     if (!authorizationToken) {
-      return res.status(500).json({ message: 'Authorization token not available' });
+      console.log('Authorization token not available');
+      return;
     }
-    // Fetch all train details from the John Doe Railway Server
+
     const headers = { Authorization: authorizationToken };
     const response = await axios.get(`${API_URL}/trains`, { headers });
     const trains = response.data;
-    
-    // console.log(trains);
 
-    if (!Array.isArray(trains)) {
-      throw new Error('Invalid response format');
-    }
+    // Filter out trains departing in the next 30 minutes
+    const currentDateTime = new Date();
+    const twelveHoursLater = new Date(currentDateTime.getTime() + 12 * 60 * 60 * 1000);
+    const filteredTrains = trains.filter((train) => {
+      const departureTime = new Date();
+      departureTime.setHours(train.departureTime.Hours, train.departureTime.Minutes, train.departureTime.Seconds);
+      return departureTime >= currentDateTime && departureTime <= twelveHoursLater;
+    });
 
-     // Filter out trains departing in the next 30 minutes
-     const currentDateTime = new Date();
-     const twelveHoursLater = new Date(currentDateTime.getTime() + 12 * 60 * 60 * 1000);
-     const filteredTrains = trains.filter((train) => {
-       const departureTime = new Date();
-       departureTime.setHours(train.departureTime.Hours, train.departureTime.Minutes, train.departureTime.Seconds);
-       return departureTime >= currentDateTime && departureTime <= twelveHoursLater;
-     });
- 
-     // Calculate actual departure time after considering delays
-     filteredTrains.forEach((train) => {
-       if (train.delayedBy) {
-         const delayMinutes = train.delayedBy;
-         const departureTime = new Date();
-         departureTime.setHours(train.departureTime.Hours, train.departureTime.Minutes, train.departureTime.Seconds);
-         departureTime.setMinutes(departureTime.getMinutes() + delayMinutes);
-         train.departureTime.Hours = departureTime.getHours();
-         train.departureTime.Minutes = departureTime.getMinutes();
-         train.departureTime.Seconds = departureTime.getSeconds();
-       }
-     });
- 
-     // Sort trains based on price, available tickets, and departure time
-     const sortedTrains = filteredTrains.sort((a, b) => {
-       const priceComparison = a.price.sleeper - b.price.sleeper || a.price.AC - b.price.AC;
-       const sleeperAvailabilityComparison = b.seatsAvailable.sleeper - a.seatsAvailable.sleeper;
-       const acAvailabilityComparison = b.seatsAvailable.AC - a.seatsAvailable.AC;
-       const departureTimeComparison =
-         new Date(a.departureTime.Hours, a.departureTime.Minutes, a.departureTime.Seconds) -
-         new Date(b.departureTime.Hours, b.departureTime.Minutes, b.departureTime.Seconds);
-       return priceComparison || sleeperAvailabilityComparison || acAvailabilityComparison || departureTimeComparison;
-     });
- 
-     // Prepare the response with train details
-     const trainSchedule = sortedTrains.map((train) => ({
-       trainName: train.trainName,
-       trainNumber: train.trainNumber,
-       departureTime: train.departureTime,
-       seatsAvailability: train.seatsAvailable,
-       price: train.price,
-     }));
+    // Calculate actual departure time after considering delays
+    filteredTrains.forEach((train) => {
+      if (train.delayedBy) {
+        const delayMinutes = train.delayedBy;
+        const departureTime = new Date();
+        departureTime.setHours(train.departureTime.Hours, train.departureTime.Minutes, train.departureTime.Seconds);
+        departureTime.setMinutes(departureTime.getMinutes() + delayMinutes);
+        train.departureTime.Hours = departureTime.getHours();
+        train.departureTime.Minutes = departureTime.getMinutes();
+        train.departureTime.Seconds = departureTime.getSeconds();
+      }
+    });
 
-    res.json(trainSchedule);
+    // Sort trains based on price, available tickets, and departure time
+    const sortedTrains = filteredTrains.sort((a, b) => {
+      const priceComparison = a.price.sleeper - b.price.sleeper || a.price.AC - b.price.AC;
+      const sleeperAvailabilityComparison = b.seatsAvailable.sleeper - a.seatsAvailable.sleeper;
+      const acAvailabilityComparison = b.seatsAvailable.AC - a.seatsAvailable.AC;
+      const departureTimeComparison =
+        new Date(a.departureTime.Hours, a.departureTime.Minutes, a.departureTime.Seconds) -
+        new Date(b.departureTime.Hours, b.departureTime.Minutes, b.departureTime.Seconds);
+      return priceComparison || sleeperAvailabilityComparison || acAvailabilityComparison || departureTimeComparison;
+    });
+
+    trainSchedule = sortedTrains;
+
+    console.log('Train schedules updated');
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.error('Failed to fetch train schedules:', error.message);
   }
+};
+
+// Fetch the authorization token and train schedules on server startup
+fetchAuthorizationToken();
+fetchTrainSchedules();
+
+// Schedule periodic train schedule updates (every 1 hour in this example)
+cron.schedule('0 * * * *', fetchTrainSchedules);
+
+// Cache train schedules for a specified duration (5 minutes in this example)
+const cacheDuration = 5 * 60 * 1000; // 5 minutes
+let cacheTimestamp = 0;
+let cachedData = [];
+
+// Middleware to check if cached data is valid
+const checkCache = (req, res, next) => {
+  const currentTime = Date.now();
+  if (currentTime - cacheTimestamp <= cacheDuration) {
+    // Return cached data if still valid
+    return res.json(cachedData);
+  }
+  // Proceed to fetch fresh data if cache is expired
+  next();
+};
+
+app.get('/trains', checkCache, (req, res) => {
+  res.json(trainSchedule);
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log('Server running on port 3000');
 });
